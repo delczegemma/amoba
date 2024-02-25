b667811770fe577552fa12518c34bb725f6c1806
Emma Takács <delczegemma@gmail.com>
2024-02-25 16:36:08 +0100

    refactor: A játék osztályba szervezése
    Az egész játékhoz tartozó függvényeket osztályba szerveztem az objektum orientáltság elérése és a könnyebb továbbfejleszthetőség érdekében. Board mivel a class scope-ján belül elérhető, kikerült a függvények argumentiumai közül.

--- src/index.js
+++ src/index.js
@@ -1,205 +1,193 @@
+class Game {
+  constructor(cells, message, restartBtn) {
+    this.cells = cells
+    this.message = message
+    this.restartBtn = restartBtn
+    this.player = "X";
+    this.board = this.initBoard();
+    this.initGame();
+  }
+  initBoard(){
+    let boardSize = Math.sqrt(this.cells.length);
+    return Array(boardSize).fill().map(() =>
+        Array(boardSize).fill(""));
+  }
+  initGame() {
+    this.cells.forEach((cell) => {
+      cell.addEventListener("click", this.makeMove.bind(this));
+    });
+    this.restartBtn.addEventListener("click", this.restartGame.bind(this));
+  }
+  makeMove(event) {
+    const cell = event.target;
+    if (cell.classList.contains('filled')) {
+      // ha már van itt lépés, ne tegyen semmit
+      return;
+    }
+
+    const row = parseInt(cell.getAttribute("cellIndexX"));
+    const col = parseInt(cell.getAttribute("cellIndexY"));
+
+    //leteszi a lépést a táblára és a tömbbe
+    this.placeStepOnBoard(cell, row, col);
+    console.log(board);
+
+    //A táblán lévő cellára filled classt rak
+    //CheckWin
+    const winDirections= this.checkWin(row, col, 4 )
+    if (winDirections){
+      const winningCoordinates = this.getWinningCells(row, col, this.player, ...winDirections);
+      this.highlightWinLine(winningCoordinates)
+      this.gameOver();
+      this.message.textContent = `Az ${this.player} játékos nyert! Gratulálunk!`
+    }
+    //CheckDraw
+    else if (this.checkDraw()){
+      this.gameOver();
+      this.message.textContent = `A játék véget ért. Az eredmény döntetlen. Újra?`
+    } else {
+      //játékosváltás
+      this.switchPlayer();
+
+      //hover helyzet váltása
+      this.cells.forEach(cell => {
+        cell.dataset.nextMove = this.player;
+      });
+    }
+  }
+  placeStepOnBoard(cell, row, col) {
+    this.board[row][col] = this.player;
+    cell.innerText = this.player;
+    cell.classList.add('filled');
+  }
+  switchPlayer() {
+    this.player = this.player === "X" ? "O" : "X";
+  }
+  gameOver() {
+    this.cells.forEach(cell => {
+      if (!cell.classList.contains('filled')) {
+        cell.classList.add('filled')
+      }
+    })
+  }
+  restartGame() {
+    this.board = this.initBoard();
+    this.player = "X";
+    this.message.textContent = "";
+    this.cells.forEach(cell => {
+      cell.classList.remove('filled')
+      cell.textContent = ""
+    })
+  }
+  checkDirection(
+      row,
+      column,
+      PlayerSymbol,
+      rowChange,
+      colChange
+  ) {
+    let difference = 1;
+    console.log(`CheckDirection függvény: `) //esetleg még paraméterek kiíratása plusszba
+    while (
+        row + difference * rowChange < this.board.length &&
+        column + difference * colChange < this.board[0].length &&
+        row + difference * rowChange >= 0 &&
+        column + difference * colChange >= 0
+        ) {
+      console.log(`   x: ${row + difference * rowChange}, y: ${
+          column + difference * colChange}, szimbólum:  ${
+          PlayerSymbol}, érték: ${this.board[row + difference * rowChange][column + (difference * colChange)]
+      }`);
+      if (
+          this.board[row + difference * rowChange][column + (difference * colChange)] ===
+          PlayerSymbol
+      ) {
+        difference++;
+      } else {
+        break;
+      }
+    }
+    console.log(`   eredmény: ${difference-1}`)
+    return difference - 1;
+  }
+  getWinningCells(row, column, PlayerSymbol, rowC1, rowC2, colC1, colC2) {
+    const winningCells = [];
+    let x = row;
+    let y = column;
+    const withinBorders = (x, y) => x >= 0 && y >= 0 && x < this.board.length && y < this.board[0].length;
+    do {
+      winningCells.push({x, y});
+      x += rowC1;
+      y += colC1
+    } while (withinBorders(x, y) && this.board[x][y] === PlayerSymbol)
+    x = row + rowC2;
+    y = column + colC2;
+    while(withinBorders(x, y) && this.board[x][y] === PlayerSymbol){
+      winningCells.push({x, y});
+      x += rowC2;
+      y += colC2
+    }
+    //tesztelés: a kapott tömb mindig a winninglength-el megegyező hosszúságú kell legyen
+    console.log("A győztes tömb: ")
+    console.log(winningCells);
+    return winningCells;
+  }
+  highlightWinLine(coordinates){
+    coordinates.forEach(({ x, y }) => {
+      const cell = document.querySelector(`.cell[cellIndexX="${x}"][cellIndexY="${y}"]`);
+      cell.classList.add('winner-cell');
+    });
+  }
+  checkWin(row, column, winningLength) {
+    const PlayerSymbol = this.board[row][column];
+    const rowLength =
+        this.checkDirection(row, column, PlayerSymbol, 0, 1) +
+        this.checkDirection(row, column, PlayerSymbol, 0, -1) +
+        1;
+    const columnLength =
+        this.checkDirection(row, column, PlayerSymbol, 1, 0) +
+        this.checkDirection(row, column, PlayerSymbol, -1, 0) +
+        1;
+    const mDiagonalLength =
+        this.checkDirection(row, column, PlayerSymbol, 1, 1) +
+        this.checkDirection(row, column, PlayerSymbol, -1, -1) +
+        1;
+    const oppDiagonalLength =
+        this.checkDirection(row, column, PlayerSymbol, 1, -1) +
+        this.checkDirection(row, column, PlayerSymbol, -1, 1) +
+        1;
+    console.log(`Checkwin függvény 
+      sora: ${rowLength},oszlopa: ${columnLength},főátló: ${mDiagonalLength},ellenátló: ${oppDiagonalLength} `)
+
+    switch (true){
+      case (rowLength >= winningLength):
+        return [0, 0, 1, -1]
+      case (columnLength >= winningLength):
+        return [1, -1, 0, 0]
+      case (mDiagonalLength >= winningLength):
+        return [1, -1, 1 ,-1]
+      case (oppDiagonalLength >= winningLength):
+        return [1, -1, -1, 1]
+      default:
+        return null;
+    }
+  }
+  checkDraw() {
+    for (let i = 0; i < this.board.length; i++) {
+      for (let j = 0; j < this.board.length; j++) {
+        console.log(`Az ${i} ${j}edik elem: ${this.board[i][j]}. Üres?`);
+        console.log(this.board[i][j] === "");
+        if (this.board[i][j] === "") {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+}
+
 const cells = document.querySelectorAll(".cell");
 const message = document.querySelector("#reportMessage");
 const restartBtn = document.querySelector("#restartBtn");
-
-let player = "X";
-
-let board = initBoard();
-
-
-function initBoard(){
-  let boardSize = Math.sqrt(cells.length);
-  return Array(boardSize).fill().map(() =>
-      Array(boardSize).fill(""));
-}
-
-initGame();
-
-function initGame(){
-  cells.forEach(cell =>
-    {
-      cell.addEventListener('click', makeMove)
-    }
-  )
-  restartBtn.addEventListener('click', restartGame);
-}
-
-function makeMove() {
-  if (this.classList.contains('filled')) {
-    // ha már van itt lépés, ne tegyen semmit
-    return;
-  }
-
-  const row = parseInt(this.getAttribute("cellIndexX"));
-  const col = parseInt(this.getAttribute("cellIndexY"));
-
-  //leteszi a lépést a táblára és a tömbbe
-  placeStepOnBoard(this, row, col);
-  console.log(board);
-
-  //A táblán lévő cellára filled classt rak
-  //CheckWin
-  const winDirections= checkWin(board, row, col, 4 )
-  if (winDirections){
-    const winningCoordinates =getWinningCells(board, row, col, player, ...winDirections);
-    highlightWinLine(winningCoordinates)
-    gameOver();
-    message.textContent = `Az ${player} játékos nyert! Gratulálunk!`
-  }
-  //CheckDraw
-  else if (checkDraw(board)){
-    gameOver();
-    message.textContent = `A játék véget ért. Az eredmény döntetlen. Újra?`
-  } else {
-    //játékosváltás
-    switchPlayer();
-
-    //hover helyzet váltása
-    cells.forEach(cell => {
-      cell.dataset.nextMove = player;
-    });
-  }
-}
-
-function placeStepOnBoard(cell, row, col) {
-  board[row][col] = player;
-  cell.innerText = player;
-  cell.classList.add('filled');
-}
-function switchPlayer() {
-  player = player === "X" ? "O" : "X";
-}
-
-function gameOver() {
-  cells.forEach(cell => {
-    if (!cell.classList.contains('filled')) {
-      cell.classList.add('filled')
-    }
-  })
-}
-
-function restartGame() {
-  board = initBoard();
-  player = "X";
-  message.textContent = "";
-  cells.forEach(cell => {
-    cell.classList.remove('filled')
-    cell.textContent = ""
-  })
-}
-
-function checkDirection(
-  board,
-  row,
-  column,
-  PlayerSymbol,
-  rowChange,
-  colChange
-) {
-  let difference = 1;
-  console.log(`CheckDirection függvény: `) //esetleg még paraméterek kiíratása plusszba
-  while (
-    row + difference * rowChange < board.length &&
-    column + difference * colChange < board[0].length &&
-    row + difference * rowChange >= 0 &&
-    column + difference * colChange >= 0
-  ) {
-    console.log(`   x: ${row + difference * rowChange}, y: ${
-      column + difference * colChange}, szimbólum:  ${
-      PlayerSymbol}, érték: ${board[row + difference * rowChange][column + (difference * colChange)]
-     }`);
-    if (
-      board[row + difference * rowChange][column + (difference * colChange)] ===
-      PlayerSymbol
-    ) {
-      difference++;
-    } else {
-      break;
-    }
-  }
-  console.log(`   eredmény: ${difference-1}`)
-  return difference - 1;
-}
-
-function getWinningCells(board, row, column, PlayerSymbol, rowC1, rowC2, colC1, colC2) {
-  const winningCells = [];
-  let x = row;
-  let y = column;
-  const withinBorders = (x, y) => x >= 0 && y >= 0 && x < board.length && y < board[0].length;
-  do {
-    winningCells.push({x, y});
-    x += rowC1;
-    y += colC1
-  } while (withinBorders(x, y) && board[x][y] === PlayerSymbol)
-  x= row + rowC2;
-  y= column + colC2;
-  while(withinBorders(x, y) && board[x][y] === PlayerSymbol){
-    winningCells.push({x, y});
-    x += rowC2;
-    y += colC2
-  }
-  //tesztelés: a kapott tömb mindig a winninglength-el megegyező hosszúságú kell legyen
-  console.log("A győztes tömb: ")
-  console.log(winningCells);
-  return winningCells;
-}
-
-function highlightWinLine(coordinates){
-  coordinates.forEach(({ x, y }) => {
-    const cell = document.querySelector(`.cell[cellIndexX="${x}"][cellIndexY="${y}"]`);
-    cell.classList.add('winner-cell');
-  });
-}
-function checkWin(board, row, column, winningLength) {
-  const PlayerSymbol = board[row][column];
-
-  const rowLength =
-    checkDirection(board, row, column, PlayerSymbol, 0, 1) +
-    checkDirection(board, row, column, PlayerSymbol, 0, -1) +
-    1;
-  const columnLength =
-    checkDirection(board, row, column, PlayerSymbol, 1, 0) +
-    checkDirection(board, row, column, PlayerSymbol, -1, 0) +
-    1;
-  const mDiagonalLength =
-    checkDirection(board, row, column, PlayerSymbol, 1, 1) +
-    checkDirection(board, row, column, PlayerSymbol, -1, -1) +
-    1;
-  const oppDiagonalLength =
-    checkDirection(board, row, column, PlayerSymbol, 1, -1) +
-    checkDirection(board, row, column, PlayerSymbol, -1, 1) +
-    1;
-  console.log(`Checkwin függvény 
-      sora: ${rowLength},oszlopa: ${columnLength},főátló: ${mDiagonalLength},ellenátló: ${oppDiagonalLength} `)
-
-  let winningCoordinates = [];
-  switch (true){
-    case (rowLength >= winningLength):
-      return [0, 0, 1, -1]
-    case (columnLength >= winningLength):
-      return [1, -1, 0, 0]
-    case (mDiagonalLength >= winningLength):
-      return [1, -1, 1 ,-1]
-    case (oppDiagonalLength >= winningLength):
-      return [1, -1, -1, 1]
-    default:
-      return null;
-  }
-}
-
-
-//console.log(checkWin(board, 2,3, 4));
-
-
-function checkDraw(board) {
-  for (let i = 0; i < board.length; i++) {
-    for (let j = 0; j < board.length; j++) {
-      console.log(`Az ${i} ${j}edik elem: ${board[i][j]}. Üres?`);
-      console.log(board[i][j] === "");
-      if (board[i][j] === "") {
-        return false;
-      }
-    }
-  }
-  return true;
-}
+const game = new Game(cells, message, restartBtn);
+game.initGame()
